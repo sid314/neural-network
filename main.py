6b1e92b3d8e14e7e14ec1fdda63608b4b160e4d5
sid314 <sidaa314@gmail.com>
2025-09-18 02:22:57 +0530

    fixed indentation error

--- main.py
+++ main.py
@@ -8,75 +8,69 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(x, y) for x, y in zip(sizes[:-1], sizes[1:])]
 
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [
+            w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)
+        ]
+        self.biases = [
+            b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)
+        ]
+
+    def sgd(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        n = len(training_data)
+        for j in range(epochs):
+            np.random.shuffle(training_data)
+            mini_batches = [
+                test_data[k : k + mini_batch_size] for k in range(0, n, mini_batch_size)
+            ]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                n_test = len(test_data)
+                print("Epoch {0}:{1}/{2}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {0} complete".format(j))
+
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) + b)
+        return a
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x]
+
+        z_vector = []
+
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            z_vector.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+
+        delta = cost_derivative(activations[-1], y) / sigmoid_derivative(z_vector[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for i in range(2, self.num_layers):
+            z = z_vector[-i]
+            s_dash = sigmoid_derivative(z)
+            delta = np.dot(self.weights[-i + 1].transpose(), delta) * s_dash
+            nabla_b[-i] = delta
+            nabla_w[-i] = np.dot(delta, activations[-i - 1].transpose())
+        return (nabla_b, nabla_w)
+
 
 def sigmoid(z):
     return 1.0 / (1 + np.exp(-z))
-
-
-def feedforward(self, a):
-    for b, w in zip(self.biases, self.weights):
-        a = sigmoid(np.dot(w, a) + b)
-    return a
-
-
-def stochastic_gradient_descent(
-    self, training_data, epochs, mini_batch_size, eta, test_data=None
-):
-    n = len(training_data)
-    for j in range(epochs):
-        np.random.shuffle(training_data)
-        mini_batches = [
-            test_data[k : k + mini_batch_size] for k in range(0, n, mini_batch_size)
-        ]
-        for mini_batch in mini_batches:
-            self.update_mini_batch(mini_batch, eta)
-        if test_data:
-            n_test = len(test_data)
-            print("Epoch {0}:{1}/{2}".format(j, self.evaluate(test_data), n_test))
-        else:
-            print("Epoch {0} complete".format(j))
-
-
-def update_mini_batch(self, mini_batch, eta):
-    nabla_b = [np.zeros(b.shape) for b in self.biases]
-    nabla_w = [np.zeros(w.shape) for w in self.weights]
-    for x, y in mini_batch:
-        delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-        nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-        nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-    self.weights = [
-        w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)
-    ]
-    self.biases = [
-        b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)
-    ]
-
-
-def backprop(self, x, y):
-    nabla_b = [np.zeros(b.shape) for b in self.biases]
-    nabla_w = [np.zeros(w.shape) for w in self.weights]
-    activation = x
-    activations = [x]
-
-    z_vector = []
-
-    for b, w in zip(self.biases, self.weights):
-        z = np.dot(w, activation) + b
-        z_vector.append(z)
-        activation = sigmoid(z)
-        activations.append(activation)
-
-    delta = cost_derivative(activations[-1], y) / sigmoid_derivative(z_vector[-1])
-    nabla_b[-1] = delta
-    nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-
-    for i in range(2, self.num_layers):
-        z = z_vector[-i]
-        s_dash = sigmoid_derivative(z)
-        delta = np.dot(self.weights[-i + 1].transpose(), delta) * s_dash
-        nabla_b[-i] = delta
-        nabla_w[-i] = np.dot(delta, activations[-i - 1].transpose())
-    return (nabla_b, nabla_w)
 
 
 def cost_derivative(output_activations, y):
